ff892922b0e09804946c7ea147fc047a2d41b01d
Mostafa23 <mostafa.m.abuayash@gmail.com>
2025-10-18 15:52:52 +0300

    update
    finished footer logging pages

--- src/app/components/Widget/footer/footer.component.ts
+++ src/app/components/Widget/footer/footer.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule, CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  email: string = '';
+  isSubscribed: boolean = false;
 
+  subscribe() {
+    this.isSubscribed = true;
+    setTimeout(() => {
+      this.isSubscribed = false;
+      this.email = '';
+    }, 2000);
+  }
 }
