7e51330566a88661e5e51f8fe93f5b26804d9392
MarwanOmar99 <mrwan99.omar@gmail.com>
2025-10-21 22:23:28 +0300

    edite courses card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import * as AOS from 'aos';
 
 import { HeaderComponent } from './components/Widget/header/header.component';
 import { FooterComponent } from './components/Widget/footer/footer.component';
@@ -7,12 +8,15 @@ import { FooterComponent } from './components/Widget/footer/footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    HeaderComponent,
-    FooterComponent
-  ],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  ngOnInit(): void {
+    AOS.init({
+      duration: 2000,
+      once: false,
+    });
+  }
+}
