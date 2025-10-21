716cb3c5b16a6feab4ad08b29d88c50892052a6a
MarwanOmar99 <mrwan99.omar@gmail.com>
2025-10-21 16:23:12 +0300

    Add : courses page & courses card

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,7 @@ import { ResetComponent } from './components/Auth/reset/reset.component';
 import { StudentaccComponent } from './components/Auth/studentacc/studentacc.component';
 import { SearchComponent } from './components/search/search.component';
 import { ListOFCoursesComponent } from './components/Courses/list-of-courses/list-of-courses.component';
+import { CoursesPageComponent } from './components/Courses/courses-page/courses-page.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -18,6 +19,6 @@ export const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'reset', component: ResetComponent },
   { path: 'student', component: StudentaccComponent },
-  { path: 'courses', component: ListOFCoursesComponent },
-  { path: 'search', component: SearchComponent }
+  { path: 'courses', component: CoursesPageComponent },
+  { path: 'search', component: SearchComponent },
 ];
